b0841e6c42b8059564ac5e347340f53fa15d9c74
PuraFome <samuelpe06062000@gmail.com>
2022-07-29 23:14:52 -0300

    ex 09

--- aula1/ex9.c
+++ aula1/ex9.c
@@ -15,8 +15,11 @@ int main(void){
     b = &a;
     c = &b;
 
-    printf(" Address of a: %p value: %d\n", &a, a);
-    printf(" Address of b: %p value: %p\n", &b, b);
-    printf(" Address of c: %p value: %p\n", &c, c);
+    printf(" Address of a: %p value: %d\n\n", &a, a);
+    printf(" Address of b: %p value: %p\n\n", &b, b);
+    printf(" Value pointed to by a: %d\n\n", *b);
+    printf(" Address of c: %p value: %p\n\n", &c, c);
+    printf(" Value pointed pointed to by c: %d\n\n", **c);
+    printf(" Value pointed to by c: %p\n\n",*c);
 
 }
